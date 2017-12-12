6f586bee991e18db4f8ef14a5249f267bab84f84
Bohee Park <boheepark91@gmail.com>
2017-12-12 10:25:44 -0500

    init

---
+++ users-service/project/__init__.py
@@ -0,0 +1,16 @@
+# users-service/project/__init__.py
+
+
+from flask import Flask, jsonify
+
+
+# instantiate the app
+app = Flask(__name__)
+
+
+@app.route('/users/ping', methods=['GET'])
+def ping_pong():
+    return jsonify({
+        'status': 'success',
+        'message': 'pong!'
+    })
