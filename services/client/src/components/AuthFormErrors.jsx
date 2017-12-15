290436e6f7ed23ecadfbb3af5ef7b241fd67daff
Bohee Park <boheepark91@gmail.com>
2017-12-15 12:24:09 -0500

    refactored

--- services/client/src/components/AuthFormErrors.jsx
+++ services/client/src/components/AuthFormErrors.jsx
@@ -4,32 +4,36 @@ import './AuthFormErrors.css';
 
 
 const AuthFormErrors = (props) => {
-    return (
-        <div>
-            <ul className="validation-list">
-                {
-                    // eslint-disable-next-line
-                    props.formRules.map((rule) => {
-                        if (rule.field === 'username') {
-                            if (props.formType === 'signup') {
-                                return <li
-                                    className={rule.valid ? "success" : "error"}
-                                    key={rule.id}
-                                >{rule.message}
-                                </li>
-                            }
-                        } else {
-                            return <li
-                                className={rule.valid ? "success" : "error"}
-                                key={rule.id}
-                            >{rule.message}
-                            </li>
-                        }
-                    })
-                }
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <ul className="validation-list">
+        {
+          // eslint-disable-next-line
+          props.formRules.map((rule) => {
+            if (rule.field === 'username') {
+              if (props.formType === 'signup') {
+                return (
+                  <li
+                    className={rule.valid ? "success" : "error"}
+                    key={rule.id}
+                  >{rule.message}
+                  </li>
+                );
+              }
+            } else {
+              return (
+                <li
+                  className={rule.valid ? "success" : "error"}
+                  key={rule.id}
+                >{rule.message}
+                </li>
+              );
+            }
+          });
+        }
+      </ul>
+    </div>
+  );
 };
 
 export default AuthFormErrors;
