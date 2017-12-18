eb7ae76eb4591f2f0541fe2ddceef1f302490746
Bohee Park <boheepark91@gmail.com>
2017-12-18 15:46:03 -0500

    auth form error

---
+++ services/client/src/components/authForm/AuthFormErrors.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import './AuthFormErrors.css';
+
+
+const AuthFormErrors = (props) => {
+  return (
+    <div>
+      <ul className="validation-list">
+        {
+          // eslint-disable-next-line
+          props.formRules.map((rule) => {
+            if (rule.field === 'username') {
+              if (props.formType === 'signup') {
+                return (
+                  <li
+                    className={rule.valid ? "success" : "error"}
+                    key={rule.id}
+                  >{rule.message}
+                  </li>
+                );
+              }
+            } else {
+              return (
+                <li
+                  className={rule.valid ? "success" : "error"}
+                  key={rule.id}
+                >{rule.message}
+                </li>
+              );
+            }
+          })
+        }
+      </ul>
+    </div>
+  );
+};
+
+export default AuthFormErrors;
