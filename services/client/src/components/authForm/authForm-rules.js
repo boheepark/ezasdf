72fd787723a270255fd456900479b4254440e227
Bohee Park <boheepark91@gmail.com>
2017-12-18 15:46:10 -0500

    auth form rules

---
+++ services/client/src/components/authForm/authForm-rules.js
@@ -0,0 +1,41 @@
+export const signupFormRules = [
+  {
+    id: 1,
+    field: 'username',
+    message: 'Username must be greater than 5 characters.',
+    valid: false
+  },
+  {
+    id: 2,
+    field: 'email',
+    message: 'Email must be greater than 5 characters.',
+    valid: false
+  },
+  {
+    id: 3,
+    field: 'email',
+    message: 'Email must be a valid email address.',
+    valid: false
+  },
+  {
+    id: 4,
+    field: 'password',
+    message: 'Password must be greater than 10 characters.',
+    valid: false
+  }
+];
+
+export const signinFormRules = [
+  {
+    id: 1,
+    field: 'email',
+    message: 'Email is required.',
+    valid: false
+  },
+  {
+    id: 2,
+    field: 'password',
+    message: 'Password is required.',
+    valid: false
+  }
+];
