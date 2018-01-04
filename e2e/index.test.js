012d434c28631c938e7672aab13692bd0429da72
Bohee Park <boheepark91@gmail.com>
2018-01-04 18:23:24 -0500

    TEST_URL

--- e2e/index.test.js
+++ e2e/index.test.js
@@ -7,7 +7,7 @@ fixture('/').page(`${TEST_URL}/`);
 
 test(`should display the page correctly if a user is not signed in`, async (t) => {
   await t
-    .navigateTo(TEST_URL)
+    .navigateTo('/')
     .expect(Selector('H1').withText('All Users').exists).ok()
     .expect(Selector('a').withText('Profile').exists).notOk()
     .expect(Selector('a').withText('Signout').exists).notOk()
