b0e7c5a0892362123b3e5c2a088199c9b72608e4
Bohee Park <boheepark91@gmail.com>
2018-01-08 06:41:04 -0500

    refactored

--- docker-push.sh
+++ docker-push.sh
@@ -6,13 +6,13 @@ then
 
   export TAG=$TRAVIS_BRANCH
 
-  # if [ "$TAG" == "dev" ];
+  # if [ "$TRAVIS_BRANCH" == "dev" ];
   # then
   #   docker login -u $DOCKER_ID -p $DOCKER_PASSWORD
   #   export REPO=$DOCKER_ID
   # fi
 
-  if [ "$TAG" == "stage" ] || [ "$TAG" == "prod" ];
+  if [ "$TRAVIS_BRANCH" == "stage" ] || [ "$TRAVIS_BRANCH" == "prod" ];
   then
     curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
     unzip awscli-bundle.zip
@@ -23,22 +23,22 @@ then
     export REPO=$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
   fi
 
-  # if [ "$TAG" == "stage" ];
+  # if [ "$TRAVIS_BRANCH" == "stage" ];
   # then
   #   export REACT_APP_USERS_SERVICE_URL="TBD"
   #   export SECRET_KEY="secret"
   # fi
   #
-  # if [ "$TAG" == "prod" ];
+  # if [ "$TRAVIS_BRANCH" == "prod" ];
   # then
   #   export REACT_APP_USERS_SERVICE_URL="TBD"
   #   export SECRET_KEY="TBD"
   # fi
 
-  if [ "$TAG" == "stage" ] || [ "$TAG" == "prod" ];
+  if [ "$TRAVIS_BRANCH" == "stage" ] || [ "$TRAVIS_BRANCH" == "prod" ];
   then
     # users
-    docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-$TAG
+    docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-$DOCKER_ENV
     docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
     docker push $REPO/$USERS:$TAG
     # users db
@@ -46,11 +46,11 @@ then
     docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
     docker push $REPO/$USERS_DB:$TAG
     # client
-    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$TAG --build arg REACT_APP_USERS_SERVICE_URL=TBD
+    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build arg REACT_APP_USERS_SERVICE_URL=TBD
     docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
     docker push $REPO/$CLIENT:$TAG
     # swagger
-    docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-$TAG $SWAGGER_DIR
+    docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-$DOCKER_ENV $SWAGGER_DIR
     docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
     docker push $REPO/$SWAGGER:$TAG
     # nginx
