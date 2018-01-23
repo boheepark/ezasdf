877f7535d8e52a6d619414d625489624df646b46
Bohee Park <boheepark91@gmail.com>
2018-01-23 15:30:48 -0500

    Dockerfile-$DOCKER_ENV -> $DOCKERFILE

--- docker-push.sh
+++ docker-push.sh
@@ -25,19 +25,17 @@ then
   if [ "$TRAVIS_BRANCH" == "stage" ];
   then
     export REACT_APP_USERS_SERVICE_URL="http://ezasdf-stage-alb-1029481067.us-east-1.elb.amazonaws.com"
-    # export SECRET_KEY="secret"
   fi
   #
   # if [ "$TRAVIS_BRANCH" == "prod" ];
   # then
   #   export REACT_APP_USERS_SERVICE_URL="TBD"
-  #   export SECRET_KEY="TBD"
   # fi
 
   if [ "$TRAVIS_BRANCH" == "stage" ] || [ "$TRAVIS_BRANCH" == "prod" ];
   then
     # users
-    docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-$DOCKER_ENV
+    docker build $USERS_REPO -t $USERS:$COMMIT -f $DOCKERFILE
     docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
     docker push $REPO/$USERS:$TAG
     # users db
@@ -45,15 +43,15 @@ then
     docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
     docker push $REPO/$USERS_DB:$TAG
     # client
-    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build-arg REACT_APP_USERS_SERVICE_URL=$REACT_APP_USERS_SERVICE_URL
+    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f $DOCKERFILE --build-arg REACT_APP_USERS_SERVICE_URL=$REACT_APP_USERS_SERVICE_URL
     docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
     docker push $REPO/$CLIENT:$TAG
     # swagger
-    docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-$DOCKER_ENV $SWAGGER_DIR
+    docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f $DOCKERFILE $SWAGGER_DIR
     docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
     docker push $REPO/$SWAGGER:$TAG
     # nginx
-    # docker build $NGINX_REPO -t $NGINX:$COMMIT -f Dockerfile-$TAG
+    # docker build $NGINX_REPO -t $NGINX:$COMMIT -f $DOCKERFILE
     # docker tag $NGINX:$COMMIT $REPO/$NGINX:$TAG
     # docker push $REPO/$NGINX:$TAG
   fi
