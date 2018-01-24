05b9ec7e1e8dc3dab3cfcd2e405637e32d24ed16
Bohee Park <boheepark91@gmail.com>
2018-01-23 22:29:11 -0500

    aws login

--- docker-pull.sh
+++ docker-pull.sh
@@ -2,10 +2,15 @@
 
 
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  if [ "$TRAVIS_BRANCH" == "stage" ] || [ "$TRAVIS_BRANCH" == "prod" ]; then
+  curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+  unzip awscli-bundle.zip
+  ./awscli-bundle/install -b ~/bin/aws
+  export PATH=~/bin:$PATH
+  eval $(aws ecr get-login --region us-east-1 --no-include-email)
+  # if [ "$TRAVIS_BRANCH" == "stage" ] || [ "$TRAVIS_BRANCH" == "prod" ]; then
     docker pull $REPO/$USERS:$TAG
     docker pull $REPO/$USERS_DB:$TAG
     docker pull $REPO/$CLIENT:$TAG
     docker pull $REPO/$SWAGGER:$TAG
-  fi
+  # fi
 fi
